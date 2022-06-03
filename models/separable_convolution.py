5939be089b0dfd005f28089d3aaf812a5df7b49e
Andreas Haglund <andreas.haglund@agens.no>
2022-06-03 15:27:21 +0200

    Add SeparableConv1D and dropout to ET encoder

---
+++ models/separable_convolution.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+from torch import Tensor
+
+
+class SeparableConv1D(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(SeparableConv1D, self).__init__()
+
+        self.depth_wise = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=kernel_size,
+            padding="same",
+            groups=in_channels
+        )
+        self.point_wise = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1
+        )
+
+    def forward(self, x: Tensor):
+        out = self.depth_wise(x)
+        out = self.point_wise(out)
+        return out
